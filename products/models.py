c99a6367cd2815d6c9c8bf4cce9b572985d56c3d
조예슬 <eungang3@naver.com>
2022-07-18 20:20:36 +0900

    Add: created products model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from categories.models import SecondCategory
+
+class Product(models.Model):
+    title = models.CharField(max_length=45)
+    description = models.TextField()
+    second_category = models.ForeignKey(SecondCategory, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+
+    class Meta:
+        db_table = 'products'
