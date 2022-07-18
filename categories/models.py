5723af7e0517b43d2c1841f5f7bbe3c9d6966b2b
조예슬 <eungang3@naver.com>
2022-07-18 20:16:55 +0900

    Add: created categories model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class FirstCategory(models.Model):
+    title = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'first_categories'
+
+class SecondCategory(models.Model):
+    title = models.CharField(max_length=50)
+    first_category = models.ForeignKey(FirstCategory, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'second_categories'
