f7ffe35be862e9ba59b872e115ad66bf3566f486
조예슬 <eungang3@naver.com>
2022-07-19 23:56:21 +0900

    Edit: edited views.py - edited to handle errors

--- categories/views.py
+++ categories/views.py
@@ -6,11 +6,15 @@ from categories.models       import FirstCategory
 class CategoryView(View): 
     # http -v GET 127.0.0.1:8000/categories
     def get(self, request):
-        first_categories = FirstCategory.objects.all()
-        result = {}
+        try:
+            first_categories = FirstCategory.objects.all()
+            result = {}
 
-        for first_category in first_categories:
-            second_categories = first_category.second_categories.all()
-            result[first_category.title] = [second_category.title for second_category in second_categories]
-       
-        return JsonResponse({'message' : result}, status=200)
+            for first_category in first_categories:
+                second_categories = first_category.second_categories.all()
+                result[first_category.title] = [second_category.title for second_category in second_categories]
+        
+            return JsonResponse({'result' : result}, status=200)
+
+        except:
+            return JsonResponse({'result' : 'INVALID_REQUEST'}, status=404)
