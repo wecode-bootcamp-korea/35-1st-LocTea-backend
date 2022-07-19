3fde99e32310461a7f634219d4471ada5c653764
조예슬 <eungang3@naver.com>
2022-07-20 01:27:09 +0900

    Edit: edited views.py - completed get 1

--- products/views.py
+++ products/views.py
@@ -1,10 +1,33 @@
-from itertools import product
 from django.http        import JsonResponse
 from django.views       import View
+
 from products.models    import Product, ThumbnailImage, DetailImage
+from categories.models  import FirstCategory, SecondCategory
 
 class ProductItemView(View):
     # http -v GET 127.0.0.1:8000/products/2
     def get(self, request, **kwargs):
-        product_id = kwargs['product_id']
-        return JsonResponse({'message': product_id}, status=200)
+        try:
+            product_id = kwargs['product_id']
+            product = Product.objects.get(id=product_id)
+            second_category = SecondCategory.objects.get(id=product.second_category.id)
+
+            result = {
+                'title'           : product.title,
+                'description'     : product.description,
+                'first_category'  : second_category.first_category.title,
+                'second_category' : second_category.title,
+                'price'           : product.price,
+                'stock'           : product.stock,
+                'discount'        : product.discount,
+                'thumbnail_images': [image.url for image in product.thumbnail_images.all()],
+                'detail_images'   : [image.url for image in product.detail_images.all()]
+            }
+
+            return JsonResponse({'result': result}, status=200)
+
+        except Product.DoesNotExist:
+            return JsonResponse({'message' : 'INVALID_PRODUCT'}, status = 401)
+
+        except SecondCategory.DoesNotExist:
+            return JsonResponse({'message' : 'INVALID_SECOND_CATEGORY'}, status = 401)
