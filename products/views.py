642dfb7a5294b75c451fb8a163722f64b124668d
조예슬 <eungang3@naver.com>
2022-07-20 00:50:05 +0900

    Add: added ProductItemView to views.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from itertools import product
+from django.http        import JsonResponse
+from django.views       import View
+from products.models    import Product, ThumbnailImage, DetailImage
 
-# Create your views here.
+class ProductItemView(View):
+    # http -v GET 127.0.0.1:8000/products/2
+    def get(self, request, **kwargs):
+        product_id = kwargs['product_id']
+        return JsonResponse({'message': product_id}, status=200)
