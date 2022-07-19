384bbf3ffdd7fd2379d3cb0ca9685d2ac55bc6a3
조예슬 <eungang3@naver.com>
2022-07-19 14:53:01 +0900

    Add: added core app, TimeStampModel

--- products/models.py
+++ products/models.py
@@ -1,13 +1,15 @@
-from django.db import models
+from django.db          import models
 
-from categories.models import SecondCategory
+from categories.models  import SecondCategory
+from core.models        import TimeStampModel
 
 class Product(models.Model):
-    title = models.CharField(max_length=45)
-    description = models.TextField()
+    title           = models.CharField(max_length=45)
+    description     = models.TextField()
     second_category = models.ForeignKey(SecondCategory, on_delete=models.CASCADE)
-    price = models.IntegerField()
-    stock = models.IntegerField()
+    price           = models.DecimalField()
+    stock           = models.IntegerField()
+    discount        = models.DecimalField(null=True)
 
     class Meta:
         db_table = 'products'
