13022fbf3770869ffb1a00fa8871618c675c7995
조예슬 <eungang3@naver.com>
2022-07-20 13:25:09 +0900

    Edit: edited views.py - get lists completed 1

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from django.urls    import path
 from products.views import ProductItemView, ProductListView
 
 urlpatterns = [
-    path('/<str:first_category>/<str:second_category>/<int:product_id>', ProductItemView.as_view()),
-    path('/<str:first_category>/<str:second_category>', ProductListView.as_view())
+    path('/<int:first_category_id>/<int:second_category_id>/<int:product_id>', ProductItemView.as_view()),
+    path('/<int:first_category_id>/<int:second_category_id>', ProductListView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -34,9 +34,18 @@ class ProductItemView(View):
             return JsonResponse({'message' : 'INVALID_SECOND_CATEGORY'}, status = 401)
 
 class ProductListView(View):
-    # 형식 /products/first_category/second_category
-    # 예시 127.0.0.1:8000/products/tea-products/greentea/
-    # http -v GET 127.0.0.1:8000/products/tea-products/greentea
+    # 티 제품 / 녹차 말차 카테고리 get
+    # http -v GET 127.0.0.1:8000/products/1/2
     def get(self, request, **kwargs):
-        
-        return JsonResponse({'result': 1}, status=200)
+        products = Product.objects.filter(second_category_id=kwargs['second_category_id'])
+        result = []
+        for product in products:
+            result.append({
+                'id'              : product.id,
+                'title'           : product.title,
+                'price'           : product.price,
+                'stock'           : product.stock,
+                'thumbnail_images': [image.url for image in product.thumbnail_images.all()],
+                'types'           : [type.name for type in product.types.all()]
+            })
+        return JsonResponse({'result': result}, status=200)
