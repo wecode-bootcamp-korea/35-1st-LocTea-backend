ea97d9d1ad242b993f74f75685af99712245702e
조예슬 <eungang3@naver.com>
2022-07-20 10:14:37 +0900

    Edit: edited cart models

--- cart/models.py
+++ cart/models.py
@@ -3,7 +3,7 @@ from django.db       import models
 class Cart(models.Model) :
     user     = models.ForeignKey('users.User', on_delete=models.CASCADE)
     product  = models.ForeignKey('products.Product', on_delete=models.CASCADE)
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(default=0)
     
     class Meta :
         db_table = 'carts'
