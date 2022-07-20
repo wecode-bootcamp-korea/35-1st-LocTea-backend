41c04a0e0bd7038b56f856f0f3b7d13126a17e1c
조예슬 <eungang3@naver.com>
2022-07-20 10:54:42 +0900

    Edit: edited urls.py - changed urls

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls    import path 
 
-from products.views import ProductItemView
+from products.views import ProductItemView, ProductListView
 
 urlpatterns = [
-    path('/<int:product_id>', ProductItemView.as_view())
+    path('/<str:first_category>/<str:second_category>/<int:product_id>', ProductItemView.as_view()),
+    path('/<str:first_category>/<str:second_category>', ProductListView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -5,18 +5,19 @@ from products.models    import Product, ThumbnailImage, DetailImage
 from categories.models  import FirstCategory, SecondCategory
 
 class ProductItemView(View):
-    # http -v GET 127.0.0.1:8000/products/2
+    # 형식 /products/first_category_name/second_category_name/product_id
+    # 예시 127.0.0.1:8000/products/tea-products/greentea/2
+    # http -v GET 127.0.0.1:8000/products/tea-products/greentea/2
     def get(self, request, **kwargs):
         try:
             product_id = kwargs['product_id']
             product = Product.objects.get(id=product_id)
-            second_category = SecondCategory.objects.get(id=product.second_category.id)
 
             result = {
                 'title'           : product.title,
                 'description'     : product.description,
-                'first_category'  : second_category.first_category.title,
-                'second_category' : second_category.title,
+                'first_category'  : kwargs['first_category'],
+                'second_category' : kwargs['second_category'],
                 'price'           : product.price,
                 'stock'           : product.stock,
                 'discount'        : product.discount,
@@ -31,3 +32,11 @@ class ProductItemView(View):
 
         except SecondCategory.DoesNotExist:
             return JsonResponse({'message' : 'INVALID_SECOND_CATEGORY'}, status = 401)
+
+class ProductListView(View):
+    # 형식 /products/first_category/second_category
+    # 예시 127.0.0.1:8000/products/tea-products/greentea/
+    # http -v GET 127.0.0.1:8000/products/tea-products/greentea
+    def get(self, request, **kwargs):
+        
+        return JsonResponse({'result': 1}, status=200)
