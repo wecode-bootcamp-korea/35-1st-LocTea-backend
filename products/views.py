a59ed48299cde4700aeb73ae5dd6bb1496add9dc
조예슬 <eungang3@naver.com>
2022-07-21 11:21:24 +0900

    Edit: edited views - deleted comment

--- products/views.py
+++ products/views.py
@@ -5,8 +5,6 @@ from products.models    import Product, ThumbnailImage, DetailImage
 from categories.models  import FirstCategory, SecondCategory
 
 class ProductListView(View):
-    # 엔드포인트는 127.0.0.1:8000/products/list?category=1
-    # http -v GET 127.0.0.1:8000/products/list category==1
     def get(self, request):
         products = Product.objects.filter(second_category_id=request.GET['category'])
         result = []
