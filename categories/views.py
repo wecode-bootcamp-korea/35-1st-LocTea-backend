3c4d3f7e1d630ab3a9f7c3a9850e3d737edaedc8
조예슬 <eungang3@naver.com>
2022-07-21 17:07:31 +0900

    added list comprehension to views.py

--- categories/views.py
+++ categories/views.py
@@ -6,16 +6,14 @@ from categories.models       import FirstCategory
 class CategoryView(View): 
     def get(self, request):
         first_categories = FirstCategory.objects.all()
-        result = []
-
-        for first_category in first_categories:
-            result.append({
+        result = [{
                 'first_category_id': first_category.id,
                 'title'            : first_category.title,
                 'second_categories': [{ 
                     'second_category_id': second_category.id,
                     'title'             : second_category.title
-                    } for second_category in first_category.second_categories.all()]})
+                } for second_category in first_category.second_categories.all()]}
+                for first_category in first_categories]
 
         return JsonResponse({'result' : result}, status=200)
 
