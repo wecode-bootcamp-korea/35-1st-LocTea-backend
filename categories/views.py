20f7bfa7e68d8ebb72c38e0f6ef7b3039e720340
조예슬 <eungang3@naver.com>
2022-07-21 14:04:49 +0900

    Edit: edited views - deleted comments

--- categories/views.py
+++ categories/views.py
@@ -4,27 +4,17 @@ from django.views import View
 from categories.models       import FirstCategory
 
 class CategoryView(View): 
-    # http -v GET 127.0.0.1:8000/categories
     def get(self, request):
-        try:
-            first_categories = FirstCategory.objects.all()
-            '''
-            # 한글명만 전달
-            result = {}
-            for first_category in first_categories:
-                second_categories = first_category.second_categories.all()
-                result[first_category.title] = [second_category.title for second_category in second_categories]
-            '''
-            result = []
-            for first_category in first_categories:
-                second_categories = first_category.second_categories.all()
-                result.append({
-                    'first_category_id' : first_category.id,
-                    'title' : first_category.title,
-                    'second_categories' : [{ 'second_category_id' : second_category.id, 'title' : second_category.title } for second_category in second_categories]})
+        first_categories = FirstCategory.objects.all()
+        result = []
 
-            return JsonResponse({'result' : result}, status=200)
+        for first_category in first_categories:
+            second_categories = first_category.second_categories.all()
+            result.append({
+                'first_category_id': first_category.id,
+                'title'            : first_category.title,
+                'second_categories': [{ 'second_category_id' : second_category.id, 'title' : second_category.title } for second_category in second_categories]})
 
-        except:
-            return JsonResponse({'result' : 'INVALID_REQUEST'}, status=404)
+        return JsonResponse({'result' : result}, status=200)
+
 
