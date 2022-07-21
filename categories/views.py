6d3ac4a3fa3a0742b8c3632003b324c272f7c3d0
조예슬 <eungang3@naver.com>
2022-07-21 17:20:00 +0900

    edited views.py - deleted tabs2

--- categories/views.py
+++ categories/views.py
@@ -7,9 +7,9 @@ class CategoryView(View):
     def get(self, request):
         first_categories = FirstCategory.objects.all()
         result = [{
-                'first_category_id': first_category.id,
-                'title'            : first_category.title,
-                'second_categories': [{ 
+                'first_category_id' : first_category.id,
+                'title'             : first_category.title,
+                'second_categories' : [{ 
                 'second_category_id': second_category.id,
                 'title'             : second_category.title
                 } for second_category in first_category.second_categories.all()]}
