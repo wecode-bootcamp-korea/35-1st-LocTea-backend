552c185c8c2c24eec13655316362ea27b2b008c7
조예슬 <eungang3@naver.com>
2022-07-21 17:59:51 +0900

    edited views.py - edited tabs

--- categories/views.py
+++ categories/views.py
@@ -7,13 +7,13 @@ class CategoryView(View):
     def get(self, request):
         first_categories = FirstCategory.objects.all()
         result = [{
-                'first_category_id' : first_category.id,
-                'title'             : first_category.title,
-                'second_categories' : [{ 
+            'first_category_id' : first_category.id,
+            'title'             : first_category.title,
+            'second_categories' : [{ 
                 'second_category_id': second_category.id,
                 'title'             : second_category.title
-                } for second_category in first_category.second_categories.all()]}
-                for first_category in first_categories]
+            } for second_category in first_category.second_categories.all()]
+        } for first_category in first_categories]
 
         return JsonResponse({'result' : result}, status=200)
 
