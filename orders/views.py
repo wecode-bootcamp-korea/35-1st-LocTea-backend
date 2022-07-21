97d6ec5c79e56d1ac21908b1f4855e81f4edb565
WorkYong <kyong@WorkYongui-MacBookAir.local>
2022-07-21 23:04:54 +0900

    Add : orders view

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views    import View
+from django.http     import JsonResponse
+
+from orders.models   import Order, OrderStatus, OrderItem, OrderItemStatus
+from core.utils      import LoginDecorator
+
+class OrderView(View):
+    @LoginDecorator
+    def post(self, request):
+        data  = json.loads(request.body)
+        user = request.user
+        
