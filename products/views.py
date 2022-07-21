97b792fb193acb009da0ca35af0b3185c3409965
조예슬 <eungang3@naver.com>
2022-07-21 17:17:51 +0900

    deleted comments

--- products/views.py
+++ products/views.py
@@ -6,20 +6,6 @@ from products.models    import Product
 from categories.models  import SecondCategory
 
 class ProductListView(View):
-    '''
-    1. 쿼리 파라미터가 없는 경우 : /products/list
-        1) '티 제품(1차 카테고리, id 1)' 카테고리에 속한 상품 전체를 보여줌
-        2) 24개만 보여줌, 신상품순으로 보여줌, 전체 타입 보여줌
-    2. 1차 카테고리를 선택한 경우 : /products/list?first-category=1
-        1) 해당 카테고리에 속한 상품 전체를 보여줌
-        2) 24개만 보여줌, 신상품순으로 보여줌, 전체 타입 보여줌(티 제품을 선택한 경우)
-    3. 2차 카테고리를 선택한 경우 : /products/list?second-category=1
-        1) 해당 카테고리에 속한 상품 전체를 보여줌
-        2) 24개만 보여줌, 신상품순으로 보여줌, 전체 타입 보여줌(티 제품을 선택한 경우)
-    4. 1차 필터를 선택한 경우 : /products/list?first-category=1&sort=new-arrival
-        1)  
-
-    '''
     def get(self, request):
         category_id = request.GET['second-category']
         if not SecondCategory.objects.filter(id=category_id).exists():
