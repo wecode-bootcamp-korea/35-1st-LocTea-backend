08ef93fff662b4a1f7dfb3da9a4786b05d3b019c
조예슬 <eungang3@naver.com>
2022-07-22 13:37:41 +0900

    editing2

--- products/views.py
+++ products/views.py
@@ -12,17 +12,25 @@ class ProductListView(View):
         sort               = request.GET.get('sort', None)
         type               = request.GET.get('type', None)
 
-        result = []
-        products = None 
+        q = Q()
 
+        '''
         # 쿼리 파라미터가 없는 경우 
         if not request.GET:
             # 1차 카테고리 아이디 1번에 속한 모든 제품을 신상품 순으로 보여줌 
-            products = Product.objects.filter(second_category__first_category_id=1).order_by('-created_at')
-        
+            q = Q(second_category__first_category_id=1).order_by('-created_at')
+        '''
+        # 1차 카테고리를 선택한 경우
         if first_category_id:
-
+            q &= Q(second_category__first_category_id = first_category_id)
         
+        # 2차 카테고리를 선택한 경우
+        if second_category_id:
+            q &= Q(second_category = second_category_id)
+
+        result = []
+        products = Product.objects.filter(q)
+    
         for product in products:
             result.append({
                 'id'              : product.id,
