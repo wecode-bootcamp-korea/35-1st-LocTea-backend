d9d7b863e6d79972aa860af4b51b127fac718a4c
조예슬 <eungang3@naver.com>
2022-07-22 12:23:34 +0900

    edited views.py - added request.GET

--- products/views.py
+++ products/views.py
@@ -7,13 +7,24 @@ from categories.models  import SecondCategory
 
 class ProductListView(View):
     def get(self, request):
-        category_id = request.GET['second-category']
-        if not SecondCategory.objects.filter(id=category_id).exists():
-            return JsonResponse({'result': 'INVALID_CATEGORY'}, status=404)
+        first_category_id  = request.GET.get('first-category', None)
+        second_category_id = request.GET.get('second-category', None)
+        sort               = request.GET.get('sort', None)
+        type               = request.GET.get('type', None)
 
         result = []
+        products = None 
+
+        # 쿼리 파라미터가 없는 경우 
+        if not request.GET:
+            # 1차 카테고리 아이디 1번에 속한 모든 제품을 신상품 순으로 보여줌 
+            products = Product.objects.filter(second_category__first_category_id=1).order_by('-created_at')
         
-        products = Product.objects.filter(second_category_id=category_id)
+        if first_category_id:
+
+
+
+        
         for product in products:
             result.append({
                 'id'              : product.id,
@@ -23,5 +34,6 @@ class ProductListView(View):
                 'thumbnail_images': [image.url for image in product.thumbnail_images.all()],
                 'types'           : [type.name for type in product.types.all()]
             })
+
         return JsonResponse({'result': result}, status=200)
-    
+        
