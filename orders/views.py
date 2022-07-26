ca18e5180a7cee7e154cdfe4ea1f362bd92791a4
WorkYong <kyong@WorkYongui-MacBookAir.local>
2022-07-26 13:03:47 +0900

    move branch

--- orders/views.py
+++ orders/views.py
@@ -3,12 +3,31 @@ import json
 from django.views    import View
 from django.http     import JsonResponse
 
-from orders.models   import Order, OrderStatus, OrderItem, OrderItemStatus
-from core.utils      import login_decorator
+from cart.models            import Cart
+from users.models           import User
+from products.models        import Product
+from orders.models          import Order, OrderStatus, OrderItem, OrderItemStatus
+from core.utils             import login_decorator
 
 class OrderView(View):
     @login_decorator
     def post(self, request):
-        data  = json.loads(request.body)
-        user = request.user
+        try:
+            data = json.loads(request.body)    
+            product_id = data["product_id"]
+            user = data["user.id"]      
+           
+            if Cart.quantity < 1 or Cart.quantity > Product.stock :
+                    return JsonResponse({"message" : "INVALID_QUANTITY"}, status=400)
+
+                Order.objects.create(
+                    user_id         = request.user.id,
+                    stock           = product.stock
+                    quantity        = cart.quantity,
+                    price           = order['price'],
+                    order_number    = order.id,
+                    order_status_id = 
+
+    
+        
         
